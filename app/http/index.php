c12db702ecf78d31177b4d9bd720a4619899efb7
Keadkead <ke.adkead@hotmail.com>
2016-07-01 14:28:50 +0700

    commit #2

--- app/http/index.php
+++ app/http/index.php
@@ -1,11 +1,13 @@
 <!-- Section include -->
-<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+<link rel="icon" href="../../favicon.ico" type="image/x-icon"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
-<script src="semantic/dist/semantic.min.js"></script>
-<link type="text/css" rel="stylesheet" href="plugin/lightslider/dist/css/lightslider.css" />
-<script src="plugin/lightslider/dist/js/lightslider.js"></script>
-<?php require_once('style.css'); ?>
+<link rel="stylesheet" type="text/css" href="../../semantic/dist/semantic.min.css">
+<script src="../../semantic/dist/semantic.min.js"></script>
+<link type="text/css" rel="stylesheet" href="../includes/lightslider/dist/css/lightslider.css" />
+<script src="../includes/lightslider/dist/js/lightslider.js"></script>
+<?php require_once('../includes/style.css'); ?>
+<!--  test -->
+<?php require_once('category.php'); ?>
 <!--  -->
 <title> CAT :3 </title>
 <div class="ui grid container">
@@ -25,7 +27,7 @@
   </div>
   <div class="row border-head">
     <div class="column">BODY
-      <?php require_once('page3.php'); ?>
+      <?php //require_once('page3.php'); ?>
     </div>
   </div>
 </div>
