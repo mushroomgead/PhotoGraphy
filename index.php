0a514a5fbfbb1d92483dc5ead81977e1568a0f44
Gead <ke.adkead@hotmail.com>
2016-09-05 21:36:41 +0700

    edit hamberger menu

--- index.php
+++ index.php
@@ -9,9 +9,6 @@ session_start();
     <link type="text/css" rel="stylesheet" href="app/includes/style.css">
     <!-- Section include -->
     <script src="app/includes/jquery.min.js"></script>
-    <!-- <link rel="stylesheet" «type="text/css" href="/semantic/dist/semantic.min.css"> -->
-    <!-- <script src="/semantic/dist/semantic.min.js"></script> -->
-
     <link rel="stylesheet" type="text/css" href="app/includes/bootstrap-3.3.6-dist/css/bootstrap.min.css">
     <script src="app/includes/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
     <link type="text/css" rel="stylesheet" href="app/includes/responsive.css">
@@ -19,7 +16,6 @@ session_start();
     <!-- lightslider -->
     <link type="text/css" rel="stylesheet" href="app/includes/lightslider/dist/css/lightslider.css" />
     <script src="app/includes/lightslider/dist/js/lightslider.js"></script>
-
     <!-- lightGallery -->
     <link type="text/css" rel="stylesheet" href="app/includes/lightGallery/dist/css/lightgallery.min.css" />
     <script src="app/includes/lightGallery/dist/js/lightGallery.min.js"></script>
@@ -31,33 +27,16 @@ session_start();
     <script type="text/javascript">
 
     $(document).ready(function() {
-    //   var img_height1 = $("#1").height();
-    //   var img_width1  = $("#1").width();
-
-    //   var img_height2 = $("#2").height();
-    //   var img_width2  = $("#2").width();
-
-    //   console.log(img_height1);
-    //   console.log(img_width1);
-    //   console.log(img_height2);
-    //   console.log(img_width2);
-
       $('#aniimated-thumbnials').lightGallery({
           thumbnail:true,
           download:false,
           showThumbByDefault: false
       });
     });
-//         $('#section-header,.layout-left,.layout-right,.font-size-header,.nav-column').addClass("sticky");
-    // console.log($(this).width());
-    // > 480
-
 
     // $(window).scroll(function() {
     //   if ($(this).scrollTop() > 1){
-    //     if($(this).width() > 360){
     //               $('#section-header,.layout-left,.layout-right,.font-size-header,.nav-column').addClass("sticky");
-    //     }
 
     //     }
     //     else{
@@ -65,22 +44,14 @@ session_start();
     //     }
     // });
 
-
-    // document.onmousedown=disableclick;
-    // function disableclick(event)
-    // {
-    //   if(event.button==2)
-    //    {
-    //      return false;
-    //    }
-    // }
     </script>
 
     <?php 
     if(isset($_SESSION['UserData']['username'])){
       echo 'MODE:'.$_SESSION['UserData']['username']; ?>
-      
-      <a href="admin/logout.php">Click to Logout</a>
+      </br>
+      <a href="admin/index.php">admin</a></br>
+      <a href="admin/logout.php">Logout</a>
     <?php } ?>
 
       <div class="clearfix section-header" id="section-header">
@@ -90,19 +61,20 @@ session_start();
         </div> -->
 
         <ul class="clearfix font-size-body layout-right topnav" id="myTopnav">
-            <li class="icon">
-              <a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()">☰</a>
+  
+            <li class="nav-column-logo pull-left font-size-header ">VV PHOTOGRAPHER</li>
+            <li class="clearfix nav-column pull-left"><a href="#">news</a></li>
+            <li class="clearfix nav-column pull-left"><a href="?page=men">men</a></li>
+            <li class="clearfix nav-column pull-left"><a href="?page=still">still</a></li>
+            <li class="clearfix nav-column pull-left"><a href="?page=women">women</a></li>
+            <li class="clearfix nav-column pull-left"><a href="?page=portrait">portrait</a></li>
+            <li class="clearfix nav-column pull-left"><a href="?page=personal">personal</a></li>
+            <li class="clearfix nav-column pull-left"><a href="?page=etc">etc</a></li>
+            <li class="clearfix nav-column pull-left"><a href="#">bio</a></li>
+            <li class="clearfix nav-column pull-left"><a href="#">contact</a></li>
+            <li class="clearfix icon">
+              <a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()" id="icon-hambg">☰</a>
             </li>
-            <li class="nav-column pull-left font-size-header ">VV PHOTOGRAPHER</li>
-            <li class="nav-column pull-left"><a href="#">news</a></li>
-            <li class="nav-column pull-left"><a href="?page=men">men</a></li>
-            <li class="nav-column pull-left"><a href="?page=still">still</a></li>
-            <li class="nav-column pull-left"><a href="?page=women">women</a></li>
-            <li class="nav-column pull-left"><a href="?page=portrait">portrait</a></li>
-            <li class="nav-column pull-left"><a href="?page=personal">personal</a></li>
-            <li class="nav-column pull-left"><a href="?page=etc">etc</a></li>
-            <li class="nav-column pull-left"><a href="#">bio</a></li>
-            <li class="nav-column pull-left"><a href="#">contact</a></li>
         </ul>
       </div>
 
@@ -152,10 +124,14 @@ session_start();
     <div class="clearfix section-footer">
       <div class="section-left">© COPYRIGHT 2015 BY VV PHOTOGRAPHER</div>
       <div class="section-right">
-        <div class="pull-left nav-footer"><i class="fa fa-twitter" aria-hidden="true"></i>&nbsp;<span id="text-footer">Twitter&nbsp;</span>|&nbsp;</div>
-        <div class="pull-left nav-footer"><i class="fa fa-facebook-square" aria-hidden="true"></i>&nbsp;<span id="text-footer">Facebook&nbsp;</span>|&nbsp;</div>
-        <div class="pull-left nav-footer"><i class="fa fa-flickr" aria-hidden="true"></i>&nbsp;<span id="text-footer">Flickr&nbsp;</span>|&nbsp;</div>
-        <div class="pull-left nav-footer"><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;<span id="text-footer">e-mail@hotmail.com</div>
+        <div class="pull-left nav-footer">
+          <i class="fa fa-twitter" aria-hidden="true"></i>&nbsp;<span id="text-footer">Twitter&nbsp;</span>|&nbsp;</div>
+        <div class="pull-left nav-footer">
+          <i class="fa fa-facebook-square" aria-hidden="true"></i>&nbsp;<span id="text-footer">Facebook&nbsp;</span>|&nbsp;</div>
+        <div class="pull-left nav-footer">
+          <i class="fa fa-flickr" aria-hidden="true"></i>&nbsp;<span id="text-footer">Flickr&nbsp;</span>|&nbsp;</div>
+        <div class="pull-left nav-footer">
+          <i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;<span id="text-footer">e-mail@hotmail.com</div>
       </div>
     </div>
     <!--  -->
