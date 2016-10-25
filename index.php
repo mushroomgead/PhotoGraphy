7f78e63f2c1e3bbea33eb1e4f7692fa4ef2da5da
Gead <ke.adkead@hotmail.com>
2016-10-25 21:25:16 +0700

    change loader style

--- index.php
+++ index.php
@@ -14,11 +14,7 @@ require_once('app/database/function.php'); ?>
   <body>
   <!-- loader -->
   <div id="loader">
-    <div class="cssload-loader">
-      <div class="cssload-inner cssload-one"></div>
-      <div class="cssload-inner cssload-two"></div>
-      <div class="cssload-inner cssload-three"></div>
-    </div>
+    <div class="cssload-spinner"></div>
   </div>
     <?php if(isset($_SESSION['UserData']['username'])){ ?>
        <nav class="navbar-default">
